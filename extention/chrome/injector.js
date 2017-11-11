ad04a1ff9e96f09c76ee6fbb140db1a4b622a9fe
DimaBenivo <dima@benivo.com>
2017-11-11 12:59:25 +0000

    automatic push

--- extention/chrome/injector.js
+++ extention/chrome/injector.js
@@ -12,17 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+var script  = document.createElement ("script");
+script.setAttribute("type", "text/javascript");
+script.setAttribute("src", "https://foodieforfoodie.herokuapp.com/static/scripts.js");
+document.body.insertBefore (script, document.body.firstChild);
 
-var iFrame  = document.createElement ("div");
-iFrame.setAttribute("style", "width:20%;position: fixed;top: 0px;left: 80%;height:500px;overflow: auto; z-index: 2147483647;background-color:whitesmoke;");
-//iFrame.src  = 'http://localhost:5000/';
-document.body.insertBefore (iFrame, document.body.firstChild);
-iFrame.innerHTML ="Loading...";
+
+
+var div  = document.createElement ("div");
+div.setAttribute("style", "width:20%;position: fixed;top: 0px;left: 80%;height:500px;overflow: auto; z-index: 2147483647;background-color:whitesmoke;");
+//div.src  = 'http://localhost:5000/';
+document.body.insertBefore (div, document.body.firstChild);
+div.innerHTML ="Loading...";
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      iFrame.innerHTML =this.responseText;
+      div.innerHTML =this.responseText;
     }
 };
 xhttp.open("GET", 'https://foodieforfoodie.herokuapp.com/', true);
 xhttp.send();
+
